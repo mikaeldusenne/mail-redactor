0e0f06009493b4766c053b20528e67aed4f38468
Mikaël Dusenne <mika@localhost.localdomain>
2017-05-25 11:51:48 +0200

    findHeader working

--- Parse_mail.hs
+++ Parse_mail.hs
@@ -1,8 +1,6 @@
 module Parse_mail where
 -- module Parse_mail (parseHeaders,  selectHeader, create_answer_mail, Mail (..)) where
 
-
-
 import Data.Char
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as Ch8 (pack, unpack)
@@ -14,7 +12,6 @@ import System.IO
 import Text.Regex.PCRE
 
 -- 
--- data Mail_header = MH [String]
 type Mail_header = [String]
 
 data Mail_content = MC { getH :: [Mail_header],
@@ -23,52 +20,40 @@ data Mail_content = MC { getH :: [Mail_header],
                     | Mail_Leaf [String]
 
 instance Show Mail_content where
-  show (MC { getH = h, getContent = b}) = (unlines .  map unlines h) ++ show b
+  show (MC { getH = h, getContent = b}) = (unlines . (++[]) . map (notrailingln . unlines) $ h) ++ (unlines . map show) b
   show (Mail_Leaf l) = unlines l
-  -- show (MC { getH = h, getContent = b}) = unlines ["§§§§§§§§§§§§§§§§",
-  --                                                  "----Headers:----",
-  --                                                  unlines . map unlines $  h,
-  --                                                  "----Content:----",
-  --                                                  unlines $ map (("§ "++) . show) b]
-
--- instance Read Mail_content where
---   readsPrec d r = 
 
 isMultipartDeclaration = isInfixOf "content-type: multipart" . map toLower
 
-findHeader h = safe_head . filter (isInfixOf h . map toLower . head)
-
--- extractMultipartBoundary s = filter (not . (=='"')) . drop 9 . dropWhile (not . isPrefixOf "boundary=")
-
--- readMultipartContent boundary content = f content
---   where f ('-':'-':xs) | isInfixOf boundary xs = 
-
--- read_mailcontent = f []
---   where f acc ("":ls) = Mail_content {getHeaders = MH acc,
---                                       getContent = read_mailcontent ls}
---         f acc (l:ls) | isMultipartDeclaration l = 
+findHeader :: String -> [[String]] -> Maybe [String]
+-- findHeader _ _ = Nothing
+-- findHeader _ [[]] = Nothing
+findHeader h l  = safe_head . grp $ l
+  where grp = filter (and . ([(>0).count, isInfixOf h . head]<*>) . pure) 
         
+
+
 readMail :: [String] -> Mail_content
 readMail l = MC {getH = h,
                  getContent = b}
    where (h',b') = sliceOn [] l
          h = readHeaders h'
          multih = findHeader "content-type: multipart" h
-         b = case multih of (Just mh) -> readMultipartContent (head mh) b'
+         b = case multih of (Just []) -> error "wtf"
+                            (Just mh) -> readMultipartContent (head mh) b'
                             Nothing -> [Mail_Leaf b']
-                         
-        -- readmail acc (l:xs)
-        --   | isEmpty l                             = (acc, [Mail_Leaf xs])
-        --   | readMultipartDeclaration l /= Nothing = (acc, readMultipartContent l xs)
-        --   | otherwise                             = readmail (acc ++ [l]) xs
 
 -- todo use fold
 readHeaders :: [String] -> [Mail_header] -- a.k.a. [[String]]
-readHeaders = rh []
-  where rh acc [] = acc
-        rh [] (l:ls) = rh [[l]] ls
-        rh acc@(a:as) (l:ls) | isHeader l = rh (acc++[[l]]) ls
-                             | otherwise  = rh ((a++[l]):as) ls
+readHeaders = spansplit isHeader
+
+-- readHeaders :: [String] -> [Mail_header] -- a.k.a. [[String]]
+-- readHeaders = rh []
+--   where rh acc [] = acc
+--         rh [] (l:ls) = rh [[l]] ls
+--         rh acc@(a:as) (l:ls) | isHeader l = rh (acc++[[l]]) ls
+--                              | otherwise  = rh ((a++[l]):as) ls
+
 
 splitHeader = safe_sliceOn ':'
 
@@ -139,114 +124,115 @@ takeBody = filter (not . lineIsHeader)
 -- lineIsHeader l = any ((flip isPrefixOf).map toLower$l) mail_headers
 lineIsHeader = (=~"^[a-zA-Z0-9\\-]+:") 
 
-parseHeaders l = map (cutHeaders []) $ groupHeaders l []
-  where groupHeaders [] acc = acc
-        groupHeaders ("":_) acc = acc
-        groupHeaders (l:ls) [] = groupHeaders ls [l]
-        groupHeaders (l:ls) acc
-          | lineIsHeader l = groupHeaders ls $ l:acc
-          | otherwise      = groupHeaders ls $ (head acc ++ l) : (tail acc)
+-- parseHeaders l = map (cutHeaders []) $ groupHeaders l []
+--   where groupHeaders [] acc = acc
+--         groupHeaders ("":_) acc = acc
+--         groupHeaders (l:ls) [] = groupHeaders ls [l]
+--         groupHeaders (l:ls) acc
+--           | lineIsHeader l = groupHeaders ls $ l:acc
+--           | otherwise      = groupHeaders ls $ (head acc ++ l) : (tail acc)
 
-        cutHeaders acc "" = (acc,"")
-        cutHeaders acc (':':' ':xs) = (acc,xs)
-        cutHeaders acc (':':xs) = (acc,xs)
-        cutHeaders acc (x:xs)  = cutHeaders (acc++[x]) xs
+--         cutHeaders acc "" = (acc,"")
+--         cutHeaders acc (':':' ':xs) = (acc,xs)
+--         cutHeaders acc (':':xs) = (acc,xs)
+--         cutHeaders acc (x:xs)  = cutHeaders (acc++[x]) xs
 
-parsePointyBrackets = f []
-  where f acc [] = acc
-        f acc ('<':ls) = f (("<" ++ a ++ ">"):acc) b
-          where (a,b) = sliceOn '>' ls
-        f acc (_:ls) = f acc ls
+-- parsePointyBrackets = f []
+--   where f acc [] = acc
+--         f acc ('<':ls) = f (("<" ++ a ++ ">"):acc) b
+--           where (a,b) = sliceOn '>' ls
+--         f acc (_:ls) = f acc ls
 
--- split comma-separated string -> [string]
-parseComma = f [] []
-  where f acc [] [] = acc
-        f acc mail [] = f (acc ++ [mail]) "" ""
-        f acc mail (',':xs) = f (acc ++ [mail]) "" xs
-        f acc mail ('"':xs) = f acc (mail ++ ('"':a++['"'])) b
-          where (a,b) = taketoquote [] xs
-                taketoquote acc [] = (acc,"")
-                taketoquote acc ('"':xs) = (acc,xs)
-                taketoquote acc (x:xs) = taketoquote (acc ++ [x]) xs
-        f acc mail (x:xs) = f acc (mail++[x]) xs
+-- -- split comma-separated string -> [string]
+-- parseComma = f [] []
+--   where f acc [] [] = acc
+--         f acc mail [] = f (acc ++ [mail]) "" ""
+--         f acc mail (',':xs) = f (acc ++ [mail]) "" xs
+--         f acc mail ('"':xs) = f acc (mail ++ ('"':a++['"'])) b
+--           where (a,b) = taketoquote [] xs
+--                 taketoquote acc [] = (acc,"")
+--                 taketoquote acc ('"':xs) = (acc,xs)
+--                 taketoquote acc (x:xs) = taketoquote (acc ++ [x]) xs
+--         f acc mail (x:xs) = f acc (mail++[x]) xs
 
-selectHeader :: String -> [(String,String)] -> String
-selectHeader h = f . g
-  where f Nothing = ""
-        f (Just (a,b)) = b
-        g = safe_head . filter ((==lowh) . toLowerStr . fst )
-        lowh = toLowerStr h
+-- selectHeader :: String -> [(String,String)] -> String
+-- selectHeader h = f . g
+--   where f Nothing = ""
+--         f (Just (a,b)) = b
+--         g = safe_head . filter ((==lowh) . toLowerStr . fst )
+--         lowh = toLowerStr h
 
-data Mail = Mail { getHeaders :: String ,
-                   getBody :: ByteString
-                 }
---             deriving Show
+-- data Mail = Mail { getHeaders :: String ,
+--                    getBody :: ByteString
+--                  }
+-- --             deriving Show
 
-instance Show Mail where
-  show (Mail {getHeaders = h, getBody = b}) = "\n======== HEADER ========\n"
-                                              ++ h
-                                              ++ "\n======== BODY ========\n"
-                                              ++ Ch8.unpack b
+-- instance Show Mail where
+--   show (Mail {getHeaders = h, getBody = b}) = "\n======== HEADER ========\n"
+--                                               ++ h
+--                                               ++ "\n======== BODY ========\n"
+--                                               ++ Ch8.unpack b
 
-create_answer_mail :: String -> Mail
-create_answer_mail = genMail . lines 
-  where genMail s = Mail {getHeaders = genHeaders . takeHeaders $ s,getBody = genBody s}
-        genBody = Ch8.pack . QP.decode . unlines . map ('>':) . takeBody
-        genHeaders s =
-          let parsedHeaders = parseHeaders s
-              parse_pointy h = parsePointyBrackets . selectHeader h $ parsedHeaders
-              parse_comma h = parseComma . selectHeader h $ parsedHeaders
-              to = [selectHeader "from" parsedHeaders]
-              original_to_cc = parse_comma "to" ++ parse_comma "cc"
-              cc = filter (not . isInfixOf "mikaeldusenne") $ original_to_cc
-              from = filter (isInfixOf "mikaeldusenne") $ original_to_cc
+-- create_answer_mail :: String -> Mail
+-- create_answer_mail = genMail . lines 
+--   where genMail s = Mail {getHeaders = genHeaders . takeHeaders $ s,getBody = genBody s}
+--         genBody = Ch8.pack . QP.decode . unlines . map ('>':) . takeBody
+--         genHeaders s =
+--           let parsedHeaders = parseHeaders s
+--               parse_pointy h = parsePointyBrackets . selectHeader h $ parsedHeaders
+--               parse_comma h = parseComma . selectHeader h $ parsedHeaders
+--               to = [selectHeader "from" parsedHeaders]
+--               original_to_cc = parse_comma "to" ++ parse_comma "cc"
+--               cc = filter (not . isInfixOf "mikaeldusenne") $ original_to_cc
+--               from = filter (isInfixOf "mikaeldusenne") $ original_to_cc
               
-              subject = (" Réponse: "++) $ selectHeader "subject" parsedHeaders
+--               subject = (" Réponse: "++) $ selectHeader "subject" parsedHeaders
                           
-              inreplyto = [selectHeader "message-id" parsedHeaders]
-              references = [selectHeader "references" parsedHeaders] ++ inreplyto
-          in unlines
-             . zipWith (++) ["To:","Cc:","From:","In-Reply-To:","References:","Subject:"]
-             . (++ [subject])
-             . (++(map concat [references])) . map (concatWith (","))
-             $ [to,cc,from,inreplyto]
+--               inreplyto = [selectHeader "message-id" parsedHeaders]
+--               references = [selectHeader "references" parsedHeaders] ++ inreplyto
+--           in unlines
+--              . zipWith (++) ["To:","Cc:","From:","In-Reply-To:","References:","Subject:"]
+--              . (++ [subject])
+--              . (++(map concat [references])) . map (concatWith (","))
+--              $ [to,cc,from,inreplyto]
 
 
 main = do
-
-  -- putStrLn $ parseComma " \"GILLIBERT, Andre\" <Andre.Gillibert@chu-rouen.fr>, mikaeldusenne@gmail.com"
+  l <- lines <$> readFile "mail_original.txt"
+  putStrLn . show $ readMail l
+--   -- putStrLn $ parseComma " \"GILLIBERT, Andre\" <Andre.Gillibert@chu-rouen.fr>, mikaeldusenne@gmail.com"
   
-  -- mail_headers <- lines.(map toLower) <$> readFile "mail_headers.txt"
-  s <- takeHeaders.lines <$> getContents
+--   -- mail_headers <- lines.(map toLower) <$> readFile "mail_headers.txt"
+--   s <- takeHeaders.lines <$> getContents
 
-  -- writeFile "body" . unlines . takeBody $ s
+--   -- writeFile "body" . unlines . takeBody $ s
 
-  hPutStrLn stderr . unlines $ s
-  hPutStrLn stderr "-----------------"
-  let parsedHeaders = parseHeaders s
-      parse_pointy h = parsePointyBrackets . selectHeader h $ parsedHeaders
-      parse_comma h = parseComma . selectHeader h $ parsedHeaders
-  -- let areHeaders = map lineIsHeader $ s 
-  -- hPutStrLn stderr $ unlines $ zipWith (\a -> \b -> show a ++ "\t" ++ show b) (map lineIsHeader s) s
+--   hPutStrLn stderr . unlines $ s
+--   hPutStrLn stderr "-----------------"
+--   let parsedHeaders = parseHeaders s
+--       parse_pointy h = parsePointyBrackets . selectHeader h $ parsedHeaders
+--       parse_comma h = parseComma . selectHeader h $ parsedHeaders
+--   -- let areHeaders = map lineIsHeader $ s 
+--   -- hPutStrLn stderr $ unlines $ zipWith (\a -> \b -> show a ++ "\t" ++ show b) (map lineIsHeader s) s
 
-  -- putStrLn . unlines . map ((++"\n").show) $ parseHeaders s
+--   -- putStrLn . unlines . map ((++"\n").show) $ parseHeaders s
            
---  putStrLn . show . parsePointyBrackets $ selectHeader "cc" parsedHeaders
-  let to = [selectHeader "from" parsedHeaders]
-      original_to_cc = parse_comma "to" ++ parse_comma "cc"
-      cc = filter (not . isInfixOf "mikaeldusenne") $ original_to_cc
-      from = filter (isInfixOf "mikaeldusenne") $ original_to_cc
+-- --  putStrLn . show . parsePointyBrackets $ selectHeader "cc" parsedHeaders
+--   let to = [selectHeader "from" parsedHeaders]
+--       original_to_cc = parse_comma "to" ++ parse_comma "cc"
+--       cc = filter (not . isInfixOf "mikaeldusenne") $ original_to_cc
+--       from = filter (isInfixOf "mikaeldusenne") $ original_to_cc
 
-      subject = (" Réponse: "++) $ selectHeader "subject" parsedHeaders
+--       subject = (" Réponse: "++) $ selectHeader "subject" parsedHeaders
       
-      inreplyto = [selectHeader "message-id" parsedHeaders]
-      references = [selectHeader "references" parsedHeaders] ++ inreplyto
---  -- debug
---  print "(((((((("
---  print $ parseHeaders ["To: <Mikaeldusenne>","In-Reply-To: someone" ]
---  print "))))))))"
---  hPutStrLn stderr $ show original_to_cc
-  putStr . unlines . zipWith (++) ["To:","Cc:","From:","In-Reply-To:","References:","Subject:"]
-    . (++ [subject])
-    . (++(map concat [references])) . map (concatWith (",")) $ [to,cc,from,inreplyto]
+--       inreplyto = [selectHeader "message-id" parsedHeaders]
+--       references = [selectHeader "references" parsedHeaders] ++ inreplyto
+-- --  -- debug
+-- --  print "(((((((("
+-- --  print $ parseHeaders ["To: <Mikaeldusenne>","In-Reply-To: someone" ]
+-- --  print "))))))))"
+-- --  hPutStrLn stderr $ show original_to_cc
+--   putStr . unlines . zipWith (++) ["To:","Cc:","From:","In-Reply-To:","References:","Subject:"]
+--     . (++ [subject])
+--     . (++(map concat [references])) . map (concatWith (",")) $ [to,cc,from,inreplyto]
 
