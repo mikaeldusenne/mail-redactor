b8e2221458546dab26fb65de9a874b3556886c33
Mikaunix <mikaunix@gmail.com>
2018-01-05 22:49:11 +0100

    attachment working, smileys working, quoted printable, Mail data

--- mredact/app/Main.hs
+++ mredact/app/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import qualified Data.Set as Set
+
+-- import qualified Codec.MIME.QuotedPrintable as QP
+import QuotedPrintable
+import Control.Lens hiding (element)
 
 import Smiley
 
@@ -8,9 +13,13 @@ import System.Process
 import System.IO
 import System.Directory
 
+import Control.Monad
+
+import Data.List
 import List
+
 import Text.Regex.PCRE
-import General (alpha_num, (!∫))
+import General
 import Hunix (exec)
 
 import Text.Pandoc
@@ -21,6 +30,9 @@ import Text.Pandoc.Walk
 import Skylighting
 import System.Random (randomIO)
 
+-- import qualified Data.ByteString.Char8 as C
+-- import 
+
 import qualified Data.Map.Strict as M
 
 getFileMimeType :: String -> IO [Char]
@@ -29,131 +41,189 @@ getFileMimeType = (extract <$>) . exec "file" . (:["--mime-type"])
 
 encode_base64 = exec "base64" . (:[])
 
-createMultiparts :: [String] -> IO ([String], [String] ,[String])
-createMultiparts ls = f ls [] [] []
-  where f [] html plain attachments = return (html, plain, attachments)
---        f (('§':xs):ls) h p a = do
-        f (l@('/':xs):ls) h p a = doesFileExist l >>= (attach !∫ default_line l ls h p a)
-          where attach = do
-                  mimeType <- getFileMimeType l
-                  b64 <- encode_base64 l
-                  -- print mimeType
-                  id <- show <$> (randomIO :: IO Integer)
-                  -- print id
-                  let is_image = beginWith "image" mimeType
-                      filename = basename l
-                      htmla=if is_image
-                            then "<img src=\"cid:" ++ id ++ "\" alt=\"image"++ id ++"\">"
-                            else ""
-                      plaina = "["++mimeType++": " ++ id ++ " ]"
+data Mail = Mail {_plain :: String
+                 ,_html :: String
+                 ,_inlines :: [String]
+                 ,_attachments :: [String]}
+makeLenses ''Mail
+
+instance Show Mail where
+  show = createAlternative
+
+instance Monoid Mail where
+  mempty = Mail { _plain="", _html="", _inlines=[], _attachments=[] }
+  mappend (Mail a b c d) (Mail a' b' c' d') = Mail (a++"\n"++a') (b++"\n"++b') (c++c') (d++d')
+
+-- create_attachment :: String -> IO (Maybe Mail)
+create_attachment path = doesFileExist path >>= (f ? return Nothing)
+  where f = do
+          mimeType <- getFileMimeType path
+          b64 <- encode_base64 path
+          id <- (("id"++) . show) <$> (randomIO :: IO Integer)
+          let filename = basename path
+              plaina = "["++mimeType++": " ++ filename ++ "@" ++ id ++ " ]"
+              contentID = "Content-ID: <" ++ id ++ ">\n"
+              contentType = ((("Content-Type: "++) . (++";")) $ mimeType)
+                          ++ " name=\"" ++ filename ++ "\"\n"
+              is_inline = beginWith "image" mimeType
+              ans = mempty {_plain=plaina}
+              atta disp= contentType
+                ++ "Content-Disposition: "++ disp ++"; filename=\""++filename++"\"\n"
+                ++ "Content-Transfer-Encoding: BASE64\n"
+                ++ contentID
+                -- ++ "X-Attachment-Id: <" ++ id ++ ">\n\n"
+                ++ "\n"
+                ++ b64
+                
+          return . Just $
+            if is_inline
+            then mempty {_html = "<img src=\"cid:" ++ id ++ "\" alt=\"image"++ id ++"\">"
+                        ,_inlines = [atta "inline"]}
+            else mempty{_attachments = [atta "attachment"]}
+
+-- createMail :: [String] -> IO Mail
+createMail ls = foldM f mempty ls
+  where f mail l@('/':_) = (mappend mail . just_or_default (default_line l))
+          <$> create_attachment (trim l)
+        f mail l = return . mappend mail $ default_line l
+        default_line l = mempty{_html=l, _plain=l}
+
+
+
+-- createMultiparts :: [String] -> IO ([String], [String] ,[String])
+-- createMultiparts ls = f ls [] [] []
+--   where f [] html plain attachments = return (html, plain, attachments)
+-- --        f (('§':xs):ls) h p a = do
+--         f (l@('/':xs):ls) h p a = doesFileExist l >>= (attach !∫ default_line l ls h p a)
+--           where attach = do
+--                   mimeType <- getFileMimeType l
+--                   b64 <- encode_base64 l
+--                   -- print mimeType
+--                   id <- (("id"++) . show) <$> (randomIO :: IO Integer)
+--                   -- print id
+--                   let is_image = beginWith "image" mimeType
+--                       filename = basename l
+--                       htmla=if is_image
+--                             then "<img src=\"cid:" ++ id ++ "\" alt=\"image"++ id ++"\">"
+--                             else ""
+--                       plaina = "["++mimeType++": " ++ filename ++ "@" ++ id ++ " ]"
                       
-                      -- contentID = if is_image then "Content-ID: <" ++ id ++ ">\n" else ""
-                      contentID = "Content-ID: <id" ++ id ++ ">\n"
-                      contentType = ((("Content-Type: "++) . (++";")) $ mimeType)
-                                    ++ " name=\"" ++ filename ++ "\"\n"
-                      contentDisposition = "Content-Disposition: "++disposition++"; filename=\"" ++ filename ++ "\"\n"
-                        where disposition = if is_image then "inline" else "attachment"
+--                       -- contentID = if is_image then "Content-ID: <" ++ id ++ ">\n" else ""
+--                       contentID = "Content-ID: <" ++ id ++ ">\n"
+--                       contentType = ((("Content-Type: "++) . (++";")) $ mimeType)
+--                                     ++ " name=\"" ++ filename ++ "\"\n"
+--                       contentDisposition = "Content-Disposition: "++disposition++"; filename=\"" ++ filename ++ "\"\n"
+--                         where disposition = if is_image then "inline" else "attachment"
                         
-                      atta = contentType
-                        ++ contentDisposition
-                        ++ "Content-Transfer-Encoding: BASE64\n"
-                        ++ contentID
-                        -- ++ "X-Attachment-Id: <" ++ id ++ ">\n\n"
-                        ++ "\n"
-                        ++ b64
-                  f ls (h++[htmla]) (p++[plaina]) (a++[atta])
+--                       atta = contentType
+--                         ++ contentDisposition
+--                         ++ "Content-Transfer-Encoding: BASE64\n"
+--                         ++ contentID
+--                         ++ "X-Attachment-Id: <" ++ id ++ ">\n\n"
+--                         ++ "\n"
+--                         ++ b64
+--                   f ls (h++[htmla]) (p++[plaina]) (a++[atta])
 
-                -- return mime++content
-        f (l:ls) h p a = default_line l ls h p a
-        default_line l ls h p a = f ls (h++[l]) (p++[l]) a
+--                 -- return mime++content
+--         f (l:ls) h p a = default_line l ls h p a
+--         default_line l ls h p a = f ls (h++[l]) (p++[l]) a
 
-createAlternative html plain attachments =
+-- data ContentType = Alternative | Mixed | Related | Plain | HTML | Attachment String
+
+-- instance Show ContentType where
+--   show Alternative = f "multipart/alternative"
+--   show Mixed       = f "multipart/mixed"
+--   show Related     = f "multipart/related"
+--   show Plain       = f "text/plain"
+--   show HTML        = f "text/html"
+--   show Attachment  = f
+--   where f s = "Content-Type: " ++ s ++ ";"
+
+-- data Encoding = QuotedPrintable | Base64
+
+-- data Mail = Multipart { content'type :: ContentType
+--                            , boundary :: String
+--                            , content :: [Mail]
+--                            }
+
+--           | Part = Part { content'transfer'encoding :: Encoding
+--                         , }
+
+createAlternative mail =
   unlines ["Content-Type: multipart/alternative; boundary=" ++ alternativeBound
-          ,"MIME-version: 1.0"
+          -- ,"MIME-version: 1.0"
           ,""
           ,alternativeBoundline ""
           ,"Content-Transfer-Encoding: quoted-printable"
-          ,"Content-Type: text/plain;"
-          ,"    charset=UTF-8;"
+          ,"Content-Type: text/plain; charset=\"UTF-8\";"
             -- ,"    format=flowed"
           ,""
-          ,plain
+          -- ,encode . _plain $ mail
+          ,_plain mail
           ,""
           ,alternativeBoundline ""
-          ,createMixed html attachments
+          ,createMixed mail
+          ,""
           ,alternativeBoundline "--"
           ]
   where alternativeBound = "alternativeboundary1234567890"
         alternativeBoundline add = "--"++alternativeBound++add
 
-createRelated html = unlines ["Content-Type: multipart/related;"
-                             ,"    type=text/html;"
+createMixed mail = unlines ["Content-Type: multipart/mixed;"
+                           ,"    type=\"text/html\";"
+                           ,"    boundary=" ++ mixedBoundary
+                           ,""
+                           ,mixedBoundaryline ""
+                           ,createRelated mail
+                           ,""
+                           ,unlines
+                             . insertBeforeEach (mixedBoundaryline "")
+                             . _attachments
+                             $ mail
+                           ,""
+                           ,mixedBoundaryline "--"
+                           ]
+  where mixedBoundary = "mixedboundary7894561230"
+        mixedBoundaryline add = "--" ++ mixedBoundary ++ add
+
+
+
+createRelated mail = unlines ["Content-Type: multipart/related;"
+                             ,"    type=\"text/html\";"
                              ,"    boundary=" ++ relatedBoundary
                              ,""
                              ,relatedBoundaryline ""
                              ,"Content-Transfer-Encoding: quoted-printable"
-                             ,"Content-Type: text/html;"
-                             ,"    charset=UTF-8;"
+                             ,"Content-Type: text/html; charset=\"UTF-8\";"
                              ,""
-                             ,html
-                             -- ,createAlternative html plain
-                             -- ,""
-                             -- ,unlines . insertBeforeEach (relatedBoundaryline "") $ attachts
+                             -- ,encode . _html $ mail
+                             ,_html mail
+                             ,""
+                             ,unlines
+                               . insertBeforeEach (relatedBoundaryline "")
+                               . _inlines $ mail
+                             ,""
                              ,relatedBoundaryline "--"
                              ]
   where relatedBoundary = "relatedBoundary7894561230"
         relatedBoundaryline add = "--" ++ relatedBoundary ++ add
 
 
-createMixed html attachts = unlines ["Content-Type: multipart/mixed;"
-                                    ,"    type=text/html;"
-                                    ,"    boundary=" ++ mixedBoundary
-                                    ,""
-                                    ,mixedBoundaryline ""
-                                    ,createRelated html
-                                    ,""
-                                    ,unlines . insertBeforeEach (mixedBoundaryline "") $ attachts
-                                    ,mixedBoundaryline "--"
-                                    ]
-  where mixedBoundary = "mixedBoundary7894561230"
-        mixedBoundaryline add = "--" ++ mixedBoundary ++ add
-
-
-
-main = do
-  ls <- lines <$> (getContents >>= substitute_smileys)
-  css <- readFile "/home/mika/.perso/pandoc.css" -- "pandoc.css"
-  -- r <- encode_base64 "/home/mika/Documents/random/lambda.png"
-  -- let (h, p, a) = createMultiparts ls
-  -- attachments <- sequence a
-  (h, p, attachments) <- createMultiparts ls
+run str = do
+  ls <- lines <$> substitute_smileys str
+  
   template <- readFile "/home/mika/.perso/pandoc.html"
-  let md_str = unlines h
-      plain = unlines p
-      readerExts = Set.union (Set.fromList [Ext_emoji]) $ readerExtensions def
+  let readerExts = Set.union (Set.fromList [Ext_emoji]) $ readerExtensions def
       readerOpts = (def {readerStandalone = True,
                          readerExtensions = readerExts})
       writerOpts = (def {writerHighlight = True,
                          writerHighlightStyle = tango,
                          writerTemplate = Just template})
-      (Right pandoc) = readMarkdown
-                       readerOpts
-                       md_str
-      html_str = writeHtmlString writerOpts pandoc
-      -- html_str = unlines ["<!DOCTYPE html><html><head>",
-      --                     "<meta charset=\"UTF-8\">",
-      --                     "<style>",
-      --                     concat . lines $ css,
-      --                     "</style></head><body>",
-      --                     body_html_str,
-      --                     "</body></html>"]
+      compileHtml = writeHtmlString writerOpts . (\(Right e) -> e) . readMarkdown readerOpts
+  
+  mail <- over plain encode . over html (encode . compileHtml) <$> createMail ls
 
-  writeFile "/home/mika/tmp/mail_body.txt" html_str
-  putStrLn $ createAlternative html_str plain attachments
+  print mail
+  -- putStrLn $ createAlternative mail
 
-
-
-
--- pandoc writer for blockquote
--- surround with div?
--- { pandocWalker b@(BlockQuote l ) = Div ("",[],[]) [b]; w e = e }
+main = getContents >>= run
